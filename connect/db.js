cdc494b13b2caa6e6db27d87e0895379c6ab2e03
shehai <wshehaniif@gmail.com>
2021-03-02 12:21:24 +0530

    integrate with angular project

--- connect/db.js
+++ connect/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("feedbackmodule", "root", "1234", {
+module.exports = new Sequelize("feedbackmodule", "root", "root1", {
   host: "127.0.0.1",
   dialect: "mysql",
 });
