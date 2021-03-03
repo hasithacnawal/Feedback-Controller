95ca7236c108a365133d39bb1ab8487605bb95be
shehai <wshehaniif@gmail.com>
2021-03-03 12:42:31 +0530

    create user login interface and add ngrx

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/pages/login/login.component';
+import  {FormsModule} from '@angular/forms';
+import {StoreModule} from "@ngrx/store";
+import {EffectsModule} from '@ngrx/effects';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot(
+
+      {}),
+      EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
