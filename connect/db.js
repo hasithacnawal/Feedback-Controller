f5a980541eda38c5d63f9c93cfa1a7a8764a471e
Chana96 <ahasithachandana@gmail.com>
2021-03-16 15:03:44 +0530

    json files updated

--- connect/db.js
+++ connect/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("feedbackmodule", "root", "root1", {
+module.exports = new Sequelize("feedbackmodule", "root", "1234", {
   host: "127.0.0.1",
   dialect: "mysql",
 });
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,25 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/pages/login/login.component';
-import  {FormsModule} from '@angular/forms';
-import {StoreModule} from "@ngrx/store";
-import {EffectsModule} from '@ngrx/effects';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot(
-
-      {}),
-      EffectsModule.forRoot([]),
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
