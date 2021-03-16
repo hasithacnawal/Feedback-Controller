39384d99b4feef1b7e6279cd1fa962b2afd2017d
Chana96 <ahasithachandana@gmail.com>
2021-03-16 16:28:29 +0530

    gitignore mofified

--- frontEnd/src/app/views/pages/login/login.component.ts
+++ frontEnd/src/app/views/pages/login/login.component.ts
@@ -1,120 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Store,State,select} from '@ngrx/store';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store, State, select } from '@ngrx/store';
 import * as userProfileActions from 'src/app/auth/actions/user-profile.action';
-import * as fromUser  from 'src/app/auth/reducers/user-profile.reducer';
-import {User} from 'src/app/auth/models/user.model'
-import {AppState} from 'src/app/core/models/app-state';
-import * as userSelector from 'src/app/auth/selectors/user-profile.selector'
+import * as fromUser from 'src/app/auth/reducers/user-profile.reducer';
+import { User } from 'src/app/auth/models/user.model';
+import { AppState } from 'src/app/core/models/app-state';
+import * as userSelector from 'src/app/auth/selectors/user-profile.selector';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   userForm: FormGroup;
-error$: Observable<String>;
-sucess$: Observable<String>;
-su: string;
-//user : User={id:0,username:'',email:'',password:''}
-
-
+  error$: Observable<String>;
+  sucess$: Observable<String>;
+  su: string;
+  //user : User={id:0,username:'',email:'',password:''}
 
   constructor(
-
-    private fb : FormBuilder,
-    private store : Store<AppState>,
+    private fb: FormBuilder,
+    private store: Store<AppState>,
     private router: Router
-
-
-
-
-
-
-
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-
     this.userForm = this.fb.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required],
-
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     });
 
-   this.sucess$ = this.store.pipe(select(userSelector.getSuccess));
+    this.sucess$ = this.store.pipe(select(userSelector.getSuccess));
     this.error$ = this.store.pipe(select(userSelector.getError));
-
-
-
-
-
-
-
-
-
   }
 
-  userLogin(){
-
-
-
-
-
-    const newUser: User= {
-      id:0,
-      username:" ",
-      email: this.userForm.get("email").value,
-      password: this.userForm.get("password").value,
-
+  userLogin() {
+    const newUser: User = {
+      id: 0,
+      username: ' ',
+      email: this.userForm.get('email').value,
+      password: this.userForm.get('password').value,
     };
 
-    if(!this.userForm.valid) {
-
-      alert('All the fileds are required!')
+    if (!this.userForm.valid) {
+      alert('All the fileds are required!');
       this.userForm.reset();
-
-    }
-
-    else{
-
+    } else {
       this.store.dispatch(new userProfileActions.LoadUserLoginAction(newUser));
       //this.router.navigate(['/dashboard'])
       //this.sucess$ = this.store.pipe(select(userSelector.getSuccess));
       //this.error$ = this.store.pipe(select(userSelector.getError));
 
-     if(!this.error$==null){
-      alert('error')
+      if (!this.error$ == null) {
+        alert('error');
+      } else {
+        this.sucess$.subscribe((val) => alert(val));
 
-      }else{
-
-        this.sucess$
-        .subscribe(val => alert(val),
-                )
-
-         //alert('sucessfully login')
-        console.log(this.sucess$)
+        //alert('sucessfully login')
+        console.log(this.sucess$);
         this.userForm.reset();
 
-        this.router.navigate(['/dashboard'])
+        this.router.navigate(['/dashboard']);
 
         //console.log(this.error$ = this.store.pipe(select(userSelector.getError)));
-
       }
 
       //this.userForm.reset();
-
-
-
     }
-
-
-
-
-
   }
-
 }
