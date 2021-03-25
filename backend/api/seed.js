9ade0360bbfc1ece285474d392b2fffb72f18791
shehai <wshehaniif@gmail.com>
2021-03-25 09:48:35 +0530

    add admin for  seed.js

--- backend/api/seed.js
+++ backend/api/seed.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const admin = require("../data/admin");
 const orgs = require("../data/orgs");
 const roles = require("../data/roles");
+const Admin = require("../models/Admin");
 const Organization = require("../models/Organization");
 const Role = require("../models/role");
 const router = express.Router();
@@ -8,8 +10,11 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   const organizations = await Organization.bulkCreate(orgs);
   const role = await Role.bulkCreate(roles);
-  res.send({ organizations, role });
+  const admins =await Admin.bulkCreate(admin);
+  res.json({ organizations, role,admins });
   console.log("oranization data inserted");
+  
+  //res.send({admins});
 });
 
 module.exports = router;
