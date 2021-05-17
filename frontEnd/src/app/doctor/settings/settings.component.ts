2bedad01913f7d8e8d6aee2167b7b3573e283fcf
shehai <wshehaniif@gmail.com>
2021-05-17 15:27:57 +0530

    update admin profile

--- frontEnd/src/app/doctor/settings/settings.component.ts
+++ frontEnd/src/app/doctor/settings/settings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "src/app/core/service/auth.service";
+import {Admin} from "src/app/core/models/admin";
+import { BehaviorSubject, Observable } from "rxjs";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-settings",
@@ -7,7 +10,34 @@ import { AuthService } from "src/app/core/service/auth.service";
   styleUrls: ["./settings.component.sass"],
 })
 export class SettingsComponent implements OnInit {
-  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  public currentUserSubject:BehaviorSubject<Admin>;
+  public form: FormGroup;
+
+  admin$:Observable<Admin>;
+  constructor(private authService: AuthService) {
+
+    this.form = new FormGroup({
+      name: new FormControl(''),
+    });
+   
+  }
+
+  ngOnInit(): void {
+
+    console.log(localStorage.getItem("currentUser"));
+    this.currentUserSubject = new BehaviorSubject<Admin>(
+      JSON.parse(localStorage.getItem("currentUser"))
+    );
+
+    this.form.patchValue({
+      name: this.currentUserSubject.value.name,
+      });
+
+    console.log(this.currentUserSubject);
+  }
+
+  
+
+  
 }
